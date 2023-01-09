1d418ced69640335b7a16efce85f890279447539
maria-magdalena-bogoeva <maggiiiiii@abv.bg>
2023-01-09 13:29:07 +0200

    webapp

---
+++ CatApplication/CatApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CatApplication.Models;
+
+namespace CatApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<CatApplication.Models.Cat> Cats { get; set; }
+    }
+}
---
+++ SchoolApplication/SchoolApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SchoolApplication.Models;
+
+namespace SchoolApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<SchoolApplication.Models.Student> Students { get; set; }
+    }
+}
